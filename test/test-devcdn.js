1d4d575bd0386a874932bcf2875c3d01ba120ca6
Paul Baumgart <paul@proxv.com>
2012-05-23 21:17:41 -0700

    clean up dev cdn and add sha1 function to bundle

--- test/test-devcdn.js
+++ test/test-devcdn.js
@@ -4,9 +4,9 @@ var _ = require('underscore');
 var createDevCdn = require('../lib/dev-cdn');
 
 vows.describe('test Dev CDN').addBatch({
-  "Dev CDN with port, env, and cwd": {
+  "Dev CDN with no env": {
     topic: function() {
-      createDevCdn(4444, 'production', __dirname + '/../', this.callback);
+      createDevCdn(null, __dirname + '/../', this.callback);
     },
 
     "found bundles": function(err, bundleConfigs) {
@@ -16,14 +16,26 @@ vows.describe('test Dev CDN').addBatch({
     }
   },
 
-  "Dev CDN with port and bundleConfigs": {
+  "Dev CDN with env and cwd": {
+    topic: function() {
+      createDevCdn('production', __dirname + '/../', this.callback);
+    },
+
+    "found bundles": function(err, bundleConfigs) {
+      assert.isNull(err);
+      assert.equal(typeof bundleConfigs['jsbundle.js'], 'object');
+      assert.equal(_(bundleConfigs).keys().length, 1);
+    }
+  },
+
+  "Dev CDN with bundleConfigs": {
     topic: function() {
       var bundleConfigs = {
         abc: {
           entryFile: __dirname + '/fixtures/abc.js'
         }
       };
-      createDevCdn(4444, bundleConfigs, null, this.callback);
+      createDevCdn(bundleConfigs, null, this.callback);
     },
 
     "found bundles": function(err, bundleConfigs) {
