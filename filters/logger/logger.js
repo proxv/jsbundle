96d5a70b234f5112d9a5da37102735f99b23eb5c
Paul Baumgart <paul@proxv.com>
2012-08-15 20:46:44 -0700

    better detection for whether the console prototype hack logger will work

--- filters/logger/logger.js
+++ filters/logger/logger.js
@@ -5,9 +5,10 @@ function Logger() {
   }
 }
 
-if (typeof console !== 'undefined' && typeof console.log === 'function') {
+try {
   Logger.prototype = console;
-} else { // for the dreaded IE, which doesn't allow console.log.apply
+  (new Logger).groupEnd();
+} catch (e) {
   function getLogFunction(name) {
     return function(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) {
       if (typeof console !== 'undefined') {
